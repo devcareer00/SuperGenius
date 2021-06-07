47b7cc24b34b2afd92e588740ade9a0e64158fdf
creativeid00 <creativeid00@gmail.com>
2021-06-08 04:40:53 +0700

    Input parameters for DApp application fixed. https://gitlab.geniusventures.io:8486/GeniusVentures/GeniusSDK/-/issues/1

--- example/processing_dapp/processing_dapp.cpp
+++ example/processing_dapp/processing_dapp.cpp
@@ -1,85 +1,17 @@
-#include "processing/processing_service.hpp"
+#include "SGProcessing.pb.h"
+
+#include <libp2p/multi/multibase_codec/multibase_codec_impl.hpp>
+#include <crdt/globaldb/globaldb.hpp>
 
 #include <iostream>
 #include <boost/program_options.hpp>
 #include <boost/format.hpp>
-#include <libp2p/multi/multibase_codec/multibase_codec_impl.hpp>
-
-using namespace sgns::processing;
 
 namespace
-{
-    class ProcessingCoreImpl : public ProcessingCore
-    {
-    public:
-        ProcessingCoreImpl(size_t nSubtasks, size_t subTaskProcessingTime)
-            : m_nSubtasks(nSubtasks)
-            , m_subTaskProcessingTime(subTaskProcessingTime)
-        {
-        }
-
-        void SplitTask(const SGProcessing::Task& task, SubTaskList& subTasks) override
-        {
-            for (size_t i = 0; i < m_nSubtasks; ++i)
-            {
-                auto subtask = std::make_unique<SGProcessing::SubTask>();
-                subtask->set_ipfsblock(task.ipfs_block_id());
-                subtask->set_results_channel((boost::format("%s_subtask_%d") % task.results_channel() % i).str());
-                subTasks.push_back(std::move(subtask));
-            }
-        }
-
-        void  ProcessSubTask(
-            const SGProcessing::SubTask& subTask, SGProcessing::SubTaskResult& result,
-            uint32_t initialHashCode) override 
-        {
-            std::cout << "SubTask processing started. " << subTask.results_channel() << std::endl;
-            std::this_thread::sleep_for(std::chrono::milliseconds(m_subTaskProcessingTime));
-            std::cout << "SubTask processed. " << subTask.results_channel() << std::endl;
-            result.set_ipfs_results_data_id((boost::format("%s_%s") % "RESULT_IPFS" % subTask.results_channel()).str());
-        }
-        
-    private:
-        size_t m_nSubtasks;
-        size_t m_subTaskProcessingTime;
-    };
-
-
-    class ProcessingTaksQueueImpl : public ProcessingTaksQueue
-    {
-    public:
-        ProcessingTaksQueueImpl(const std::list<SGProcessing::Task>& tasks)
-            : m_tasks(tasks)
-        {
-        }
-
-        bool PopTask(SGProcessing::Task& task) override
-        {
-            if (m_tasks.empty())
-            {
-                return false;
-            }
-
-
-            task = std::move(m_tasks.back());
-            m_tasks.pop_back();
-
-            return true;
-        };
-
-    private:
-        std::list<SGProcessing::Task> m_tasks;
-    };
-
+{  
     // cmd line options
     struct Options 
     {
-        size_t serviceIndex = 0;
-        size_t subTaskProcessingTime = 0; // ms
-        size_t roomSize = 0;
-        size_t disconnect = 0;
-        size_t nSubTasks = 5;
-        size_t channelListRequestTimeout = 5000;
         // optional remote peer to connect to
         std::optional<std::string> remote;
     };
@@ -94,12 +26,7 @@ namespace
             po::options_description desc("processing service options");
             desc.add_options()("help,h", "print usage message")
                 ("remote,r", po::value(&remote), "remote service multiaddress to connect to")
-                ("processingtime,p", po::value(&o.subTaskProcessingTime), "subtask processing time (ms)")
-                ("roomsize,s", po::value(&o.roomSize), "subtask processing time (ms)")
-                ("disconnect,d", po::value(&o.disconnect), "disconnect after (ms)")
-                ("nsubtasks,n", po::value(&o.nSubTasks), "number of subtasks that task is split to")
-                ("channellisttimeout,t", po::value(&o.channelListRequestTimeout), "chnnel list request timeout (ms)")
-                ("serviceindex,i", po::value(&o.serviceIndex), "index of the service in computational grid (has to be a unique value)");
+                ;
 
             po::variables_map vm;
             po::store(parse_command_line(argc, argv, desc), vm);
@@ -108,24 +35,6 @@ namespace
             if (vm.count("help") != 0 || argc == 1) 
             {
                 std::cerr << desc << "\n";
-                return boost::none;
-            }
-
-            if (o.serviceIndex == 0) 
-            {
-                std::cerr << "Service index should be > 0\n";
-                return boost::none;
-            }
-
-            if (o.subTaskProcessingTime == 0)
-            {
-                std::cerr << "SubTask processing time should be > 0\n";
-                return boost::none;
-            }
-
-            if (o.roomSize == 0)
-            {
-                std::cerr << "Processing room size should be > 0\n";
                 return boost::none;
             }
 
@@ -170,64 +79,62 @@ int main(int argc, char* argv[])
 
     auto pubs = std::make_shared<sgns::ipfs_pubsub::GossipPubSub>();
 
-    if (options->serviceIndex == 1)
-    {
-        std::string publicKey = "z5b3BTS9wEgJxi9E8NHH6DT8Pj9xTmxBRgTaRUpBVox9a";
-        std::string privateKey = "zGRXH26ag4k9jxTGXp2cg8n31CEkR2HN1SbHaKjaHnFTu";
-
-        libp2p::crypto::KeyPair keyPair;
-        auto codec = libp2p::multi::MultibaseCodecImpl();
-        keyPair.publicKey = { libp2p::crypto::PublicKey::Type::Ed25519, codec.decode(publicKey).value() };
-        keyPair.privateKey = { libp2p::crypto::PublicKey::Type::Ed25519, codec.decode(privateKey).value() };
-
-        pubs = std::make_shared<sgns::ipfs_pubsub::GossipPubSub>(keyPair);
-    }
-
+    std::vector<std::string> pubsubBootstrapPeers;
     if (options->remote)
     {
-        pubs->Start(40001, { *options->remote });
+        pubsubBootstrapPeers = std::move(std::vector({ *options->remote }));
     }
-    else
-    {
-        pubs->Start(40001, {});
-    }
+
+    pubs->Start(40001, pubsubBootstrapPeers);
 
     const size_t maximalNodesCount = 1;
 
     std::list<SGProcessing::Task> tasks;
 
-    // Only a service with index equal to 1 has a locked task (job)
-    if (options->serviceIndex == 1)
-    {
-        SGProcessing::Task task;
-        task.set_ipfs_block_id("IPFS_BLOCK_ID_1");
-        task.set_block_len(1000);
-        task.set_block_line_stride(2);
-        task.set_block_stride(4);
-        task.set_random_seed(0);
-        task.set_results_channel("RESULT_CHANNEL_ID_1");
-        tasks.push_back(std::move(task));
-    }
+    // Put a single task to Global DB
+    // And wait for its processing
+    SGProcessing::Task task;
+    task.set_ipfs_block_id("IPFS_BLOCK_ID_1");
+    task.set_block_len(1000);
+    task.set_block_line_stride(2);
+    task.set_block_stride(4);
+    task.set_random_seed(0);
+    task.set_results_channel("RESULT_CHANNEL_ID_1");
+    tasks.push_back(std::move(task));
 
-    boost::asio::deadline_timer timerToDisconnect(*pubs->GetAsioContext());
-    if (options->disconnect > 0)
+    auto io = std::make_shared<boost::asio::io_context>();
+    sgns::crdt::GlobalDB globalDB(
+        io, "CRDT.Datastore.TEST", "CRDT.Datastore.TEST.Channel", 40001, pubsubBootstrapPeers);
+
+    auto crdtOptions = sgns::crdt::CrdtOptions::DefaultOptions();
+
+    globalDB.Start(crdtOptions);
+    auto dataStore = globalDB.GetDatastore();
+
+    for (auto& task : tasks)
     {
-        timerToDisconnect.expires_from_now(boost::posix_time::milliseconds(options->disconnect));
-        timerToDisconnect.async_wait([pubs, &timerToDisconnect](const boost::system::error_code& error)
+        sgns::base::Buffer valueBuffer;
+        valueBuffer.put(task.SerializeAsString());
+        auto setKeyResult = dataStore->PutKey(sgns::crdt::HierarchicalKey("TASK_1"), valueBuffer);
+        if (setKeyResult.has_failure())
         {
-            timerToDisconnect.expires_at(boost::posix_time::pos_infin);
-            pubs->Stop();
-        });
+            std::cout << "Unable to put key-value to CRDT datastore." << std::endl;
+        }
+
+        // Check if data put
+        auto getKeyResult = dataStore->GetKey(sgns::crdt::HierarchicalKey("TASK_1"));
+        if (getKeyResult.has_failure())
+        {
+            std::cout << "Unable to find key in CRDT datastore"<< std::endl;
+        }
+        else
+        {
+            std::cout << "[" << "TASK_1" << "] -> " << std::endl;
+            // getKeyResult.value().toString()
+        }
     }
 
-    auto taskQueue = std::make_shared<ProcessingTaksQueueImpl>(tasks);
-    auto processingCore = std::make_shared<ProcessingCoreImpl>(options->nSubTasks, options->subTaskProcessingTime);
-    ProcessingServiceImpl processingService(pubs, maximalNodesCount, options->roomSize, taskQueue, processingCore);
-
-    processingService.Listen(processingGridChannel);
-    processingService.SetChannelListRequestTimeout(boost::posix_time::milliseconds(options->channelListRequestTimeout));
-
-    processingService.SendChannelListRequest();
+    dataStore->PublishDelta();
 
     // Gracefully shutdown on signal
     boost::asio::signal_set signals(*pubs->GetAsioContext(), SIGINT, SIGTERM);
