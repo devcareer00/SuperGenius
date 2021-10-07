1a739388c253a3e86979ed28a07cf0079b0af48f
creativeid00 <creativeid00@gmail.com>
2021-10-08 01:03:11 +0700

    An example added for peer info obtaining from DHT. https://gitlab.geniusventures.io:8486/GeniusVentures/supergenius/-/issues/10

--- example/ipfs_client/ipfs.cpp
+++ example/ipfs_client/ipfs.cpp
@@ -1,10 +1,9 @@
-#include <crdt/globaldb/globaldb.hpp>
-#include <crdt/globaldb/keypair_file_storage.hpp>
-
 #include <libp2p/multi/multibase_codec/multibase_codec_impl.hpp>
 #include <libp2p/injector/host_injector.hpp>
+#include <libp2p/injector/kademlia_injector.hpp>
 #include <libp2p/host/host.hpp>
 #include <libp2p/protocol/common/asio/asio_scheduler.hpp>
+#include <libp2p/multi/content_identifier_codec.hpp>
 
 #include <ipfs_lite/ipfs/graphsync/graphsync.hpp>
 #include <ipfs_lite/ipfs/graphsync/impl/graphsync_impl.hpp>
@@ -63,252 +62,16 @@ namespace
         }
         return boost::none;
     }
-
-    // MerkleDAG bridge interface for test purposes
-    class TestDataService : public sgns::ipfs_lite::ipfs::graphsync::MerkleDagBridge {
-    public:
-        using Storage = std::map<sgns::CID, sgns::common::Buffer>;
-
-        TestDataService& addData(const std::string& s) {
-            insertNode(data_, s);
-            return *this;
-        }
-
-        TestDataService& addExpected(const std::string& s) {
-            insertNode(expected_, s);
-            return *this;
-        }
-
-        const Storage& getData() const {
-            return data_;
-        }
-
-        const Storage& getExpected() const {
-            return expected_;
-        }
-
-        const Storage& getReceived() const {
-            return received_;
-        }
-
-        // places into data_, returns true if expected
-        bool onDataBlock(sgns::CID cid, sgns::common::Buffer data);
-
-    private:
-        static void insertNode(Storage& dst, const std::string& data_str);
-
-        outcome::result<size_t> select(
-            const sgns::CID& cid,
-            gsl::span<const uint8_t> selector,
-            std::function<bool(const sgns::CID& cid, const sgns::common::Buffer& data)> handler)
-            const override;
-
-        Storage data_;
-        Storage expected_;
-        Storage received_;
-    };
-
-    std::pair<std::shared_ptr<sgns::ipfs_lite::ipfs::graphsync::Graphsync>, std::shared_ptr<libp2p::Host>>
-        createNodeObjects(std::shared_ptr<boost::asio::io_context> io) {
-
-        // [boost::di::override] allows for creating multiple hosts for testing
-        // purposes
-        auto injector =
-            libp2p::injector::makeHostInjector<boost::di::extension::shared_config>(
-                boost::di::bind<boost::asio::io_context>.to(
-                    io)[boost::di::override]);
-
-        std::pair<std::shared_ptr<sgns::ipfs_lite::ipfs::graphsync::Graphsync>, std::shared_ptr<libp2p::Host>>
-            objects;
-        objects.second = injector.template create<std::shared_ptr<libp2p::Host>>();
-        auto scheduler = std::make_shared<libp2p::protocol::AsioScheduler>(
-            io, libp2p::protocol::SchedulerConfig{});
-        objects.first =
-            std::make_shared<sgns::ipfs_lite::ipfs::graphsync::GraphsyncImpl>(objects.second, std::move(scheduler));
-        return objects;
-    }
-
-    bool TestDataService::onDataBlock(sgns::CID cid, sgns::common::Buffer data) {
-        bool expected = false;
-        auto it = expected_.find(cid);
-        if (it != expected_.end() && it->second == data) {
-            expected = (received_.count(cid) == 0);
-        }
-        received_[cid] = std::move(data);
-        return expected;
-    }
-
-    inline void TestDataService::insertNode(TestDataService::Storage& dst,
-        const std::string& data_str) {
-        using NodeImpl = sgns::ipfs_lite::ipld::IPLDNodeImpl;
-        auto node = NodeImpl::createFromString(data_str);
-        dst[node->getCID()] = node->getRawBytes();
-    }
-
-    inline outcome::result<size_t> TestDataService::select(
-        const sgns::CID& cid,
-        gsl::span<const uint8_t> selector,
-        std::function<bool(const sgns::CID& cid, const sgns::common::Buffer& data)> handler)
-        const {
-        auto it = data_.find(cid);
-        if (it != data_.end()) {
-            handler(it->first, it->second);
-            return 1;
-        }
-        return 0;
-    }
-
-    class Node {
-    public:
-        // total requests sent by all nodes in a test case
-        static size_t requests_sent;
-
-        // total responses received by all nodes in a test case
-        static size_t responses_received;
-
-        // n_responses_expected: count of responses received by the node after which
-        // io->stop() is called
-        Node(std::shared_ptr<boost::asio::io_context> io,
-            std::shared_ptr<sgns::ipfs_lite::ipfs::graphsync::MerkleDagBridge> data_service,
-            sgns::ipfs_lite::ipfs::graphsync::Graphsync::BlockCallback cb,
-            size_t n_responses_expected)
-            : io_(std::move(io)),
-            data_service_(std::move(data_service)),
-            block_cb_(std::move(cb)),
-            n_responses_expected_(n_responses_expected) {
-            std::tie(graphsync_, host_) = createNodeObjects(io_);
-        }
-
-        // stops graphsync and host, otherwise they can interact with further tests!
-        void stop() {
-            graphsync_->stop();
-            host_->stop();
-        }
-
-        // returns peer ID, so they can connect to each other
-        auto getId() const {
-            return host_->getId();
-        }
-
-        // listens to network and starts nodes if not yet started
-        void listen(const libp2p::multi::Multiaddress& listen_to) {
-            auto listen_res = host_->listen(listen_to);
-            if (!listen_res) {
-                //logger->trace("Cannot listen to multiaddress {}, {}",
-                //    listen_to.getStringAddress(),
-                //    listen_res.error().message());
-                return;
-            }
-            start();
-        }
-
-        // calls Graphsync's makeRequest
-        void makeRequest(const libp2p::peer::PeerId& peer,
-            boost::optional<libp2p::multi::Multiaddress> address,
-            const sgns::CID& root_cid) {
-            start();
-
-            std::vector<sgns::ipfs_lite::ipfs::graphsync::Extension> extensions;
-            sgns::ipfs_lite::ipfs::graphsync::ResponseMetadata response_metadata{};
-            sgns::ipfs_lite::ipfs::graphsync::Extension response_metadata_extension =
-                sgns::ipfs_lite::ipfs::graphsync::encodeResponseMetadata(response_metadata);
-            extensions.push_back(response_metadata_extension);
-            std::vector<sgns::CID> cids;
-            sgns::ipfs_lite::ipfs::graphsync::Extension do_not_send_cids_extension = 
-                sgns::ipfs_lite::ipfs::graphsync::encodeDontSendCids(cids);
-            extensions.push_back(do_not_send_cids_extension);
-            // unused code , request_ is deleted because Subscription have deleted copy-constructor and operator
-                  // requests_.push_back(graphsync_->makeRequest(peer,
-                  //                                             std::move(address),
-                  //                                             root_cid,
-                  //                                             {},
-                  //                                             extensions,
-                  //                                             requestProgressCallback()));
-                  // Subscription subscription = graphsync_->makeRequest(peer,
-                  //                                             std::move(address),
-                  //                                             root_cid,
-                  //                                             {},
-                  //                                             extensions,
-                  //                                             requestProgressCallback());
-            requests_.push_back(std::shared_ptr<libp2p::protocol::Subscription>(
-                new libp2p::protocol::Subscription(std::move(graphsync_->makeRequest(peer,
-                std::move(address),
-                root_cid,
-                {},
-                extensions,
-                requestProgressCallback())))));
-
-            //-------------------------------------------------------------------------------------
-
-            ++requests_sent;
-        }
-
-    private:
-        void start() {
-            if (!started_) {
-                graphsync_->start(data_service_, block_cb_);
-                host_->start();
-                started_ = true;
-            }
-        }
-
-        // helper, returns requesu callback fn
-        sgns::ipfs_lite::ipfs::graphsync::Graphsync::RequestProgressCallback requestProgressCallback() {
-            static auto formatExtensions =
-                [](const std::vector<sgns::ipfs_lite::ipfs::graphsync::Extension>& extensions) -> std::string {
-                std::string s;
-                for (const auto& item : extensions) {
-                    s += fmt::format(
-                        "({}: 0x{}) ", item.name, sgns::common::Buffer(item.data).toHex());
-                }
-                return s;
-            };
-            return [this](sgns::ipfs_lite::ipfs::graphsync::ResponseStatusCode code,
-                const std::vector<sgns::ipfs_lite::ipfs::graphsync::Extension>& extensions) {
-                    ++responses_received;
-                    //logger->trace("request progress: code={}, extensions={}",
-                    //    statusCodeToString(code),
-                    //    formatExtensions(extensions));
-                    if (++n_responses == n_responses_expected_) {
-                        io_->stop();
-                    }
-            };
-        }
-
-        // asion context to be stopped when needed
-        std::shared_ptr<boost::asio::io_context> io_;
-
-        std::shared_ptr<sgns::ipfs_lite::ipfs::graphsync::Graphsync> graphsync_;
-
-        std::shared_ptr<libp2p::Host> host_;
-
-        std::shared_ptr<sgns::ipfs_lite::ipfs::graphsync::MerkleDagBridge> data_service_;
-
-        sgns::ipfs_lite::ipfs::graphsync::Graphsync::BlockCallback block_cb_;
-
-        // keeping subscriptions alive, otherwise they cancel themselves
-        // class Subscription have non-copyable constructor and operator, so it can not be used in std::vector
-        // std::vector<Subscription> requests_;
-
-        std::vector<std::shared_ptr<sgns::ipfs_lite::ipfs::graphsync::Subscription >> requests_;
-
-        size_t n_responses_expected_;
-        size_t n_responses = 0;
-        bool started_ = false;
-    };
-
-    size_t Node::requests_sent = 0;
-    size_t Node::responses_received = 0;
-
+    
 }
 
 int main(int argc, char* argv[])
 {
-    auto options = parseCommandLine(argc, argv);
-    if (!options)
-    {
-        return 1;
-    }
+    //auto options = parseCommandLine(argc, argv);
+    //if (!options)
+    //{
+    //    return EXIT_FAILURE;
+    //}
 
     const std::string logger_config(R"(
     # ----------------
@@ -317,12 +80,12 @@ int main(int argc, char* argv[])
         type: console
         color: true
     groups:
-      - name: ipfs_client
+      - name: main
         sink: console
         level: info
         children:
           - name: libp2p
-          - name: Gossip
+          - name: kademlia
     # ----------------
     )");
 
@@ -337,98 +100,111 @@ int main(int argc, char* argv[])
 
     libp2p::log::setLoggingSystem(logging_system);
 
-    auto loggerPubSub = libp2p::log::createLogger("GossipPubSub");
-    //loggerPubSub->set_level(spdlog::level::trace);
-
     auto loggerDAGSyncer = libp2p::log::createLogger("GraphsyncDAGSyncer");
     loggerDAGSyncer->setLevel(soralog::Level::TRACE);
 
-    auto loggerBroadcaster = libp2p::log::createLogger("PubSubBroadcasterExt");
-    loggerBroadcaster->setLevel(soralog::Level::DEBUG);
-
     const std::string processingGridChannel = "GRID_CHANNEL_ID";
 
-    auto pubs = std::make_shared<sgns::ipfs_pubsub::GossipPubSub>(
-        sgns::crdt::KeyPairFileStorage("CRDT.IPFS.TEST/ipfs").GetKeyPair().value());
+    libp2p::protocol::kademlia::Config kademlia_config;
+    kademlia_config.randomWalk.enabled = true;
+    kademlia_config.randomWalk.interval = std::chrono::seconds(300);
+    kademlia_config.requestConcurency = 20;
 
-    std::vector<std::string> pubsubBootstrapPeers;
-    if (options->remote)
+    auto injector = libp2p::injector::makeHostInjector(
+        // libp2p::injector::useKeyPair(kp), // Use predefined keypair
+        libp2p::injector::makeKademliaInjector(
+            libp2p::injector::useKademliaConfig(kademlia_config)));
+
+    try
     {
-        pubsubBootstrapPeers = std::move(std::vector({ *options->remote }));
+        auto ma = libp2p::multi::Multiaddress::create("/ip4/127.0.0.1/tcp/40000").value();  // NOLINT
+
+        auto io = injector.create<std::shared_ptr<boost::asio::io_context>>();
+        auto host = injector.create<std::shared_ptr<libp2p::Host>>();
+
+        auto kademlia =
+            injector
+            .create<std::shared_ptr<libp2p::protocol::kademlia::Kademlia>>();
+
+        auto bootstrap_nodes = [] {
+            std::vector<std::string> addresses = {
+                "/ip4/202.61.244.123/tcp/4001/p2p/QmUikGKv3RysyCYZxNoGWjf8Y1qPjhrp8NeV4fYyNyqcDQ",
+            };
+            std::unordered_map<libp2p::peer::PeerId,
+                std::vector<libp2p::multi::Multiaddress>>
+                addresses_by_peer_id;
+
+            for (auto& address : addresses) {
+                auto ma = libp2p::multi::Multiaddress::create(address).value();
+                auto peer_id_base58 = ma.getPeerId().value();
+                auto peer_id = libp2p::peer::PeerId::fromBase58(peer_id_base58).value();
+
+                addresses_by_peer_id[std::move(peer_id)].emplace_back(std::move(ma));
+            }
+
+            std::vector<libp2p::peer::PeerInfo> v;
+            v.reserve(addresses_by_peer_id.size());
+            for (auto& i : addresses_by_peer_id) {
+                v.emplace_back(libp2p::peer::PeerInfo{
+                    /*.id =*/ i.first, /*.addresses =*/ {std::move(i.second)} });
+            }
+
+            return v;
+        }();
+
+        auto peer_id = libp2p::peer::PeerId::fromBase58("12D3KooWSGCJYbM6uCvCF7cGWSitXSJTgEb7zjVCaxDyYNASTa8i").value();
+
+        io->post([&] {
+            auto listen = host->listen(ma);
+            if (!listen) {
+                std::cerr << "Cannot listen address " << ma.getStringAddress().data()
+                    << ". Error: " << listen.error().message() << std::endl;
+                std::exit(EXIT_FAILURE);
+            }
+
+            for (auto& bootstrap_node : bootstrap_nodes) {
+                kademlia->addPeer(bootstrap_node, true);
+            }
+
+            host->start();
+
+            auto res = kademlia->findPeer(peer_id, [&](libp2p::outcome::result<libp2p::peer::PeerInfo> pi) {
+                if (!pi.has_failure())
+                {
+                    std::cout << "Peer found: " << pi.value().id.toBase58() << std::endl;
+                    std::cout << "Peers addresses: ";
+                    for (auto& address : pi.value().addresses)
+                    {
+                        std::cout << address.getStringAddress() << ";";
+                    }
+                }
+                else
+                {
+                    std::cout << "Peer not found. Peer: " << peer_id.toBase58()
+                        << ", Reason: " << pi.error().message() << std::endl;
+                }
+                // Say to world about his providing
+                //provide();
+
+                // Ask provider from world
+                //find_providers();
+
+                //kademlia->start();
+                });
+
+            }); // 
+
+        boost::asio::signal_set signals(*io, SIGINT, SIGTERM);
+        signals.async_wait(
+            [&io](const boost::system::error_code&, int) { io->stop(); });
+        io->run();
     }
-    pubs->Start(40001, pubsubBootstrapPeers);
-
-    //------------------------------------------------------------------------------
-    auto listen_to =
-        libp2p::multi::Multiaddress::create("/ip4/127.0.0.1/tcp/40000").value();
-
-    auto io = std::make_shared<boost::asio::io_context>();
-
-    // strings from which we create blocks and CIDs
-    std::vector<std::string> strings({ "xxx", "yyy", "zzz" });
-
-    // creating instances
-    auto server_data = std::make_shared<TestDataService>();
-
-    // server block callback expects no blocks
-    auto server_cb = [](sgns::CID, sgns::common::Buffer) { };
-
-    auto client_data = std::make_shared<TestDataService>();
-
-    // clienc block callback expect 3 blocks from the string above
-    auto client_cb = [&client_data](sgns::CID cid, sgns::common::Buffer data) {
-        if (!client_data->onDataBlock(std::move(cid), std::move(data))) {
-        }
-    };
-
-    for (const auto& s : strings) {
-        // client expects what server has
-
-        server_data->addData(s);
-        client_data->addExpected(s);
+    catch (const std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << std::endl;
+        return EXIT_FAILURE;
     }
 
-    Node server(io, server_data, server_cb, 0);
-    Node client(io, client_data, client_cb, 3);
-
-    // starting all the stuff asynchronously
-
-    io->post([&]() {
-        // server listens
-        server.listen(listen_to);
-        auto peer = server.getId();
-        bool use_address = true;
-
-        // client makes 3 requests
-
-        for (const auto& [cid, _] : client_data->getExpected()) {
-            boost::optional<libp2p::multi::Multiaddress> address(listen_to);
-
-            // don't need to pass the address more than once
-            client.makeRequest(peer, use_address ? address : boost::none, cid);
-            use_address = false;
-        }
-        });
-
-    std::thread iothread([io]() { io->run(); });
-
-    //------------------------------------------------------------------------------
-    // Gracefully shutdown on signal
-    boost::asio::signal_set signals(*pubs->GetAsioContext(), SIGINT, SIGTERM);
-    signals.async_wait(
-        [&pubs, &io](const boost::system::error_code&, int)
-        {
-            pubs->Stop();
-            io->stop();
-        });
-
-    pubs->Wait();
-
-    client.stop();
-    server.stop();
-
-    iothread.join();
-
-    return 0;
+    return EXIT_SUCCESS;
 }
 
